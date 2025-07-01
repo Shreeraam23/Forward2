2eff9cb0fc2f49e0b7519d5b4160c68c12d2b483
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-07-01 06:32:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", "20373203"))
-    API_HASH = environ.get("API_HASH", "8962717c7c708e210f66ea658db58d85")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7552442021:AAFgA-JkfEwKtg38MwPk-aglx0kCnUf5VAc") 
+    API_ID = int(environ.get("API_ID", "21500696"))
+    API_HASH = environ.get("API_HASH", "014094fdf01ca625435839b8dd7d22b7")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8063113202:AAE3gpXUeg8pNS74Frmea1tWtIg2mKwrrns") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://vevob65684:CBT55WcRjWUf0GvN@cluster0.dfd6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vevob65684")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "7968732891"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7584800556"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
