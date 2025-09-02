7ba1049d6c5102a77e87052a77936a3e86197f2e
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-09-02 10:27:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://vevob65684:CBT55WcRjWUf0GvN@cluster0.dfd6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vevob65684")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://koxiher824:uhWjdKAzmfAHcagy@cluster0.lg66r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Koxiher824")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -27,4 +27,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
